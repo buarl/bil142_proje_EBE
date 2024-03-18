7078c37c1695ccf6188c3c5b3c7e43ab54844795
buarl <114435618+buarl@users.noreply.github.com>
2024-03-18 17:00:23 +0300

    Create Gemi.h

---
+++ Gemi.h
@@ -0,0 +1,39 @@
+#ifndef DENEME_GEMI_H
+#define DENEME_GEMI_H
+#include <iostream>
+using namespace std;
+
+class Gemi {
+public:
+    int para{0};
+    int saglik{100};
+    int yakit{100};
+    int dayaniklilik{1};
+    int hiz{1};
+    int defaultYakit{33};
+
+    float hizKatsayi{1.0};
+    float saglikKatsayi{1.0};
+    float yakitKatsayi{1.0};
+    float dayaniklilikKatsayi{1.0};
+    float yakitAzalmaKatsayi{1.0};
+
+    virtual void hiz_Belirleme  () = 0 ;
+    virtual void dayaniklilik_Belirleme () = 0 ;
+    virtual void para_Belirleme () = 0 ;
+    virtual void saglik_Belirleme () = 0 ;
+    virtual void yakit_Belirleme () = 0 ;
+
+public:
+
+    int yakitOgrenme () const;
+    int saglikOgrenme() const;
+    int paraOgrenme() const;
+    void paraGuncelleme(int);
+    void yakitGuncelleme(int);
+    void saglikGuncelleme(int);
+    bool GemiKontrol() ;
+    int PuanHesaplama ();
+
+};
+#endif //DENEME_GEMI_H
