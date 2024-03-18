ca299ab2afb68ec1dae3048bb7faed7cd78cca34
buarl <114435618+buarl@users.noreply.github.com>
2024-03-18 23:42:54 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Gemi_Uret.h"
+
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+    srand(time(nullptr));
+
+    auto GemiUretici=new Gemi_Uret();
+
+}
