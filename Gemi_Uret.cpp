d3b9e934571cadef5b924e0acd4077a3d6da7a41
buarl <114435618+buarl@users.noreply.github.com>
2024-03-18 23:34:27 +0300

    Update Gemi_Uret.cpp

--- Gemi_Uret.cpp
+++ Gemi_Uret.cpp
@@ -1,4 +1,5 @@
-#include "GemiGenerator.h"
+
+#include "Gemi_Uret.h"
 #include <iostream>
 #include<memory>
 #include "Hizli_Gemi.h"
@@ -11,24 +12,24 @@ using namespace std;
 void Gemi_Uret::GemiUretenFonksiyon(int user) {
     shared_ptr<Gemi> Ship = nullptr;
     if (user == 1) {
-      Ship = make_shared<Normal_Gemi>();
+        Ship = make_shared<Normal_Gemi>();
     } else if (user == 2) {
-      Ship = make_shared<Guclu_Gemi>();
+        Ship = make_shared<Guclu_Gemi>();
     } else if (user == 3) {
-      Ship = :make_shared<Hizli_Gemi>();
-  }
+        Ship = make_shared<Hizli_Gemi>();
+    }
 
 
     EventUretenFonksiyon(Ship);
 
 }
 
-void GemiGenerator:: EventUretenFonksiyon(shared_ptr<Gemi> Ship) {
-    shared_ptr<EventGenerator>Yeni;
+void Gemi_Uret:: EventUretenFonksiyon(shared_ptr<Gemi> Ship) {
+    shared_ptr<Event_Uret>Yeni;
 
-    int deneme=KacKereEventCagrilacak;
+    int deneme=3;
     int counter_1{1};
-   
+
 
     while (!Ship->GemiKontrol() && counter_1<=deneme)
     {
@@ -42,28 +43,22 @@ void GemiGenerator:: EventUretenFonksiyon(shared_ptr<Gemi> Ship) {
     cout<<"Puan "<<Ship->PuanHesaplama()<<"\n";
 }
 
-
-
 void Gemi_Uret::userGemiUretFonksiyon()
 {
-    
+
     GemiSecimiBaslangici:
 
     cout<<"Geminizin Ne Oldugunu Giriniz. \n (Normal Gemi 1) (Guclu Gemi icin 2) (Hizli Gemi icin 3)\n";
     int GemiTur;
     cin>>GemiTur;
-    if(toplamGemiTur>=GemiTuruNumber && GemiTuruNumber>0){
+    if(int i = 1< i++; i<4){
         GemiUretenFonksiyon(GemiTur);
     }
     else
     {
         cout<<"ERROR 404 NOT FOUND!!!!\n";
-        break;
+
     }
 
 }
 
-Gemi_Uret::Gemi_Uret()
-{
-    this->userGemiUretFonksiyon();
-}
