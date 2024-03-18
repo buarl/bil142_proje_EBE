94f337139c087cd43f733b1acd8727dced5acfb1
buarl <114435618+buarl@users.noreply.github.com>
2024-03-18 16:07:25 +0300

    Create Gemi_Uret.h

---
+++ Gemi_Uret.h
@@ -0,0 +1,17 @@
+#ifndef DENEME_GEMIURET_H
+#define DENEME_GEMIURET_H
+#include "Gemi.h"
+#include"Event_Uret.h"
+
+class GemiUret {
+public:
+    GemiUret();
+protected:
+    int EventCagirmaSayisi{5};
+    void GemiUretUserFonskiyon();
+    void GemiUretenFonksiyon(int);
+    void EventUretenFonksiyon(shared_ptr<Gemi>);
+    const int totalGemiTuru{3};
+};
+
+#endif //DENEME_GEMIURET_H
