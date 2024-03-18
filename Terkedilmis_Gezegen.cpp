747e0f4634fd7914f2e54edadb7ffe9ece1dcc90
buarl <114435618+buarl@users.noreply.github.com>
2024-03-18 22:37:39 +0300

    Add files via upload

---
+++ Terkedilmis_Gezegen.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "Terkedilmis_Gezegen.h"
+#include "Uzay_Korsanlari.h"
+using namespace std;
+
+void Terkedilmis_Gezegen::uzayKorsaniAltinOlasilikFonksiyonu() {
+    int randomGeneratedNumber = rand() % 100; 
+    const int KazanilanAltin = 10;
+    const int AltinOlasilik = 50;
+
+    if (randomGeneratedNumber >= AltinOlasilik) {
+        cout << "OMG TEBRİKLER ALTIN BULDUNUZ\n";
+        BuGemi->paraGuncelleme(KazanilanAltin);
+    } else {
+        TerkEdilmisGezegendekiUzayKorsanlariIcinFonksiyon();
+    }
+}
+
+void Terkedilmis_Gezegen::TerkEdilmisGezegendekiUzayKorsanlariIcinFonksiyon() {
+    cout << "UZAY KORSANLARI IS HERE!!!!!\n";
+    auto NewUzayKorsanlariEvent = make_shared<UzayKorsanlari>(ThisGemi);
+}
+
+void Terkedilmis_Gezegen::uzayKorsaniAltin() {
+    uzayKorsaniAltinOlasilikFonksiyonu();
+}
+
+Terkedilmis_Gezegen::~Terkedilmis_Gezegen() {
+    // (Destructor) fonksiyon
+}
+
+void Terkedilmis_Gezegen::pureVirtualYapanFonksiyon() {
+    // Pure virtual fonksiyonun uygulanması
+}
+
+Terkedilmis_Gezegen::Terkedilmis_Gezegen(shared_ptr<Gemi> AlinanGemi) : ThisGemi(move(AlinanGemi)) {
+    uzayKorsaniAltin();
+}
