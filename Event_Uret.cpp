71a7ab10c6a7c7754712be247b4d2934981cdbf2
buarl <114435618+buarl@users.noreply.github.com>
2024-03-18 23:35:33 +0300

    Update Event_Uret.cpp

--- Event_Uret.cpp
+++ Event_Uret.cpp
@@ -1,38 +1,34 @@
-#include <iostream>
 #include <memory>
 #include "Asteroid_Kusagi.h"
 #include "Event_Uret.h"
-#include "UzayKorsanlari.h"
+#include "Uzay_Korsanlari.h"
 #include "Terkedilmis_Gezegen.h"
 #include<cmath>
 
 using namespace std;
 
-EventGenerator::EventGenerator(shared_ptr<Gemi> AlinanGemi ) {
-    eventGenerateEtmekIcinGemi=move(AlinanGemi);
+Event_Uret::Event_Uret(shared_ptr<Gemi> AlinanGemi ) {
+    //eventUretmekIcinGemi=move(AlinanGemi);
     this->EventUretecekFonksiyon();
 
 }
 
-void EventGenerator::EventUretecekFonksiyon() {
+void Event_Uret::EventUretecekFonksiyon() {
 
     int randomGeneratedNumber =(rand()%(TotalEventNumber))+1;
     shared_ptr<Event> Yeni = nullptr;
     if (randomGeneratedNumber == 1) {
-        
-      Yeni = make_shared<Asteroid_Kusagi>(eventIcinGemi);
-        
+
+        Yeni = make_shared<Asteroid_Kusagi>(eventIcinGemi);
+
     } else if (randomGeneratedNumber == 2) {
-      
+
         Yeni = make_shared<Uzay_Korsanlari>(eventIcinGemi);
-        
+
     } else if (randomGeneratedNumber == 3) {
-        
+
         Yeni = make_shared<Terkedilmis_Gezegen>(eventIcinGemi);
-        
+
     }
-
-
-}
 
 
