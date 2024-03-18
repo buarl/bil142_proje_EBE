bcd4c8962a2823c3802550dd980bf6d73ccb6589
buarl <114435618+buarl@users.noreply.github.com>
2024-03-18 20:13:11 +0300

    Add files via upload

--- Asteroid_Kusagi.cpp
+++ Asteroid_Kusagi.cpp
@@ -2,18 +2,18 @@
 #include "AsteroidKusagi.h"
 using namespace std;
 
-void  AsteroidKusagi::GemiHasarAldiMi(int sayac) {
-    const int İlkteAlinacakHasar{10};
+void  AsteroidKusagi::GemiHasarAldiMi(int counter) {
+    const int ilkteAlinacakHasar{10};
     int randomSayi = rand()%101;// 0-100 arasında değer verecek çünkü 101 bölümden kalanı olacak
     const double GemiHasarOlasiligi{50.0};
 
     if(randomSayi>=static_cast<int>(GemiHasarOlasiligi/(ThisGemi->hizKatsayisi))) {
-        cout<<sayac<<" Asteroid'de Hasar Alınmayacak \n ";
+        cout<<counter<<" Asteroid'de Hasar Alınmayacak \n ";
     }
       
     else{
-        cout<<sayac<<".Ihtimalde Asteroid Kusaginda Gemi Hasar aldi\n";
-        ThisGemi->saglikGuncelleme(İlkteAlinacakHasar);
+        cout<<counter<<"Asteroid'de Gemi Hasar aldi\n";
+        ThisGemi->saglikGuncelleme(ilkteAlinacakHasar);
 
         if(ThisGemi->saglikOgrenme()>=0) {
             cout<<"Geminin Sagligi \n"<<ThisGemi->saglikOgrenme()<<"\n";
@@ -25,24 +25,23 @@ void  AsteroidKusagi::GemiHasarAldiMi(int sayac) {
 
 void AsteroidKusagi::AsteroidKusagiBusinessLogicYapanFonk() {
 
-   const int GemiHasariFonksiyonuKacKereDonecek{3};
+   const int GemiHasariFonksiyonuDonme{3};
 
-    std::cout<<"Gemi Asteroid Kusagina Denk Geldi "<<GemiHasariFonksiyonuKacKereDonecek;
-    std::cout<<"kere hasar alma ihtimali var \n";
+    cout<<"Gemi Asteroid Kusaginda! "<<GemiHasariFonksiyonuDonme;
+    cout<<"hasar alabilir \n";
 
-    for(int sayac{1};sayac<=GemiHasariFonksiyonuKacKereDonecek;++sayac)
-    {
-        GemiHasarAldiMi(sayac);
-        if(BuGemi->GemiOlduMu()) {
+    for(int counter{1};counter <=GemiHasariFonksiyonuDonme;++counter) {
+        GemiHasarAldiMi(counter);
+        if(ThisGemi->GemiOlduMu()) {
           break;
         }
     }
 }
 void AsteroidKusagi::pureVirtualYapanFonksiyon() {}
 
-AsteroidKusagi::AsteroidKusagi(std::shared_ptr<Gemi>  AlinanGemi) {
+AsteroidKusagi::AsteroidKusagi(shared_ptr<Gemi>AlinanGemi) {
 
-    BuGemi=move(AlinanGemi);
+    ThisGemi=move(AlinanGemi);
     this->AsteroidKusagiBusinessLogicYapanFonk();
 
 }
